8538bd58c85427cacafc1c44790453c17ec4c027
Jasmine Sullivan <20147180@tafe.wa.edu.au>
2026-06-11 18:36:27 +0800

    chore: github pages publish ready

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+export default [
+    {
+        languageOptions: {
+            ecmaVersion: 2015,
+            globals: {
+                document: "readonly",
+                window: "readonly",
+                navigator: "readonly"
+            }
+        }
+    }
+];
